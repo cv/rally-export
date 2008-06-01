d8b1b5a503b59d04f52f09651eba035a16dcb0ba
Carlos Villela <administrator@cv-2.local>
2008-06-01 03:38:55 +0100

    migrated to merb (sudo gem install merb then rake to use) - MUCH simpler!

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), 'boot')
-
-Rails::Initializer.run do |config|
-  config.frameworks -= [ :active_record ]
-  config.action_controller.session = { :session_key => "_rally_export_session", :secret => "rally export tool rally export tool rally export tool" }
-end
-
-# edit this to point at the location where the Stories.xml file gets downloaded to
-STORIES_XML_LOCATION = "/Users/cv/Downloads/Stories.xml"
